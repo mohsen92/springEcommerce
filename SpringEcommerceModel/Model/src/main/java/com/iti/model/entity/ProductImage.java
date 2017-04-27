8b1cdae7088cb5c7a985cabde5885fe3b08e647a
samirKamalGhoneim <samir.k.ghoneim@gmail.com>
2017-04-27 10:27:56 +0200

    initia commit

---
+++ SpringEcommerceModel/Model/src/main/java/com/iti/model/entity/ProductImage.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iti.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Samir
+ */
+@Entity
+@Table(name = "productimage")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ProductImage.findAll", query = "SELECT p FROM ProductImage p")
+    , @NamedQuery(name = "ProductImage.findById", query = "SELECT p FROM ProductImage p WHERE p.id = :id")
+    , @NamedQuery(name = "ProductImage.findByUrl", query = "SELECT p FROM ProductImage p WHERE p.url = :url")})
+public class ProductImage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "url")
+    private String url;
+    @JoinColumn(name = "productId", referencedColumnName = "product_id")
+    @ManyToOne(optional = false)
+    private Product productId;
+
+    public ProductImage() {
+    }
+
+    public ProductImage(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Product getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Product productId) {
+        this.productId = productId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProductImage)) {
+            return false;
+        }
+        ProductImage other = (ProductImage) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.iti.model.entity.ProductImage[ id=" + id + " ]";
+    }
+    
+}
