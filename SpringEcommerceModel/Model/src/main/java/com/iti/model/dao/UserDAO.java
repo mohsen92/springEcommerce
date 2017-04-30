0558971b1cc30cb13e11c7a8e25a412e55c90407
sara-metwalli93 <sarametwalli93@gmail.com>
2017-04-30 22:18:59 +0200

    sara.. Usee

--- SpringEcommerceModel/Model/src/main/java/com/iti/model/dao/UserDAO.java
+++ SpringEcommerceModel/Model/src/main/java/com/iti/model/dao/UserDAO.java
@@ -6,6 +6,7 @@
 package com.iti.model.dao;
 
 import com.iti.model.entity.User;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserDAO extends CrudRepository<User, Integer>{
+      @Override
+    User findOne(Integer id);
+
+    @Override
+    List<User> findAll();
+   
+    public User findOneByEmail(String em);
+    
+    public User findOneByEmailAndPassword(String em,String password);
     
 }
